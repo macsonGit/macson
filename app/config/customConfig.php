d3eef4fadac74c46401019070b78bfbfff986297
root <root@macson.es>
2015-12-23 15:10:28 +0100

    23122015

--- app/config/customConfig.php
+++ app/config/customConfig.php
@@ -162,8 +162,8 @@ define ('COMMERCE_MANAGEMENT_EMAIL', "gfuset@macson.es");
 //Replace this URL in production mode (test: https://sis-t.redsys.es:25443/sis/realizarPago, production: https://sis.redsys.es/sis/realizarPago)
 define('SERMEPA_URL', 'https://sis.redsys.es/sis/realizarPago');
 define('SERMEPA_MERCHANT_CODE', '327234068');
-//define('SERMEPA_MERCHANT_KEY', 'sq7HjrUOBfKmC576ILgskD5srU870gJ7');
-define('SERMEPA_MERCHANT_KEY', 'erjusrtjus45usestruh');
+define('SERMEPA_MERCHANT_KEY', 'vQzswfl8rwefUO/bTvaHO8G0ulKVv0cT');
+//define('SERMEPA_MERCHANT_KEY', 'erjusrtjus45usestruh');
 //define('SERMEPA_MERCHANT_KEY', 'qwertyasdf0123456789');
 
 define('SERMEPA_CURRENCY_EQUIVALENCE', serialize(array('EUR' => '978', 'default' => '978')));
